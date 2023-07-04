92346e27cf747bbdd4c0c6880362cad441921cee
Faraz Shah <31269969+farazshah487@users.noreply.github.com>
2023-07-04 17:07:33 +0500

    updates

--- MewsConnector/src/main/java/MewsConnector/controllers/CustomException.java
+++
@@ -1,7 +0,0 @@
-package MewsConnector.controllers;
-
-public class CustomException extends RuntimeException {
-    public CustomException(String message, Throwable cause) {
-        super(message, cause);
-    }
-}
