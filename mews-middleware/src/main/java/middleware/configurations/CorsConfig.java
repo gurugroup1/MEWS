3b16cce004244606757bbe3752070456738d484d
Faraz Shah <31269969+farazshah487@users.noreply.github.com>
2023-07-05 18:10:20 +0500

    Updates

--- mews-middleware/src/main/java/middleware/configurations/CorsConfig.java
+++ mews-middleware/src/main/java/middleware/configurations/CorsConfig.java
@@ -5,13 +5,12 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class CorsConfiguration implements WebMvcConfigurer {
-
+public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:4200") // Replace with the appropriate origin
+                .allowedOrigins("http://localhost:4200")
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowedHeaders("*");
     }
-}s
+}
